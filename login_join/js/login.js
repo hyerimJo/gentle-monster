97f0c0b4f8e80a7f7a561d4f1d22380a02fb08b8
Hyerim Jo <dlapdlfspdlxm@gmail.com>
2022-08-29 17:16:56 +0900

    first commit

---
+++ login_join/js/login.js
@@ -0,0 +1,9 @@
+$(function () {
+  /*search open*/
+  $(".search_btn").click(function () {
+    $(".search_open").fadeIn();
+  });
+  $(".search_close").click(function () {
+    $(".search_open").fadeOut();
+  });
+});
